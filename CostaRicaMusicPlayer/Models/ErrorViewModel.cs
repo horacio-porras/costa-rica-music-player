f375ddede6ccdf55c1bd5b2d1e82b4a521294cf6
Horacio <157430552+hporrasm@users.noreply.github.com>
2026-02-23 17:38:40 -0600

    Base y estructura del proyecto

---
+++ CostaRicaMusicPlayer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CostaRicaMusicPlayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
